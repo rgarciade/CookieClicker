88b053c597f7e7e7ced58cd0ae4c147b1088e4e6
raul.garciad <raul.garciad@sngular.com>
2024-04-04 10:05:04 +0200

    move manifest11

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-// if (!globalThis.URLPattern) {
-// 	await import('urlpattern-polyfill');
-// }
+if (!globalThis.URLPattern) {
+	await import('urlpattern-polyfill');
+}
 import(`./router.js`);
 //prevenimos que se habilite el doble click y se amplie la pantalla en iphone
 // document.addEventListener('dblclick', function (event) {
